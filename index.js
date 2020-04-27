d359325ce906c547d300ef27028b3bac7576d840
Honluk <luhan09@qq.com>
2020-04-27 16:05:25 +0800

    update

--- index.js
+++ index.js
@@ -5,17 +5,29 @@ module.exports = {
     "plugin:prettier/recommended",
     "eslint:recommended",
     "prettier/@typescript-eslint",
-    "@vue/typescript"
+    "@vue/typescript",
   ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "prettier/prettier": [
       "error",
-      { jsxBracketSameLine: true, htmlWhitespaceSensitivity: "ignore" }
-    ]
+      { jsxBracketSameLine: true, htmlWhitespaceSensitivity: "ignore" },
+    ],
+    "no-implicit-coercion": "off",
+    "no-empty": "off",
+    "vue/require-valid-default-prop": "off",
+    "vue/require-prop-types": "off",
+    "vue/require-default-prop": "off",
+    "vue/no-unused-vars": "off",
+    "@typescript-eslint/prefer-for-of": "off",
+    "@typescript-eslint/no-var-requires": "off", //允许requires
+    "@typescript-eslint/explicit-member-accessibility": "off", //类访问修饰符
+    "@typescript-eslint/member-ordering": "off", //排序
+    "@typescript-eslint/no-parameter-properties": "off",
+    "@typescript-eslint/no-this-alias": "off",
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser"
-  }
+    parser: "@typescript-eslint/parser",
+  },
 };
